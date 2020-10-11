7d66157c5d9fa83c0a13d1d5a725ac973f96260d
Haritha Dasanayake <Haritha Dasanayake@DESKTOP-DM1CECQ>
2020-10-11 22:40:46 +0530

    Admin App created

---
+++ administer/src/components/ConfigAccount.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ConfigAccount extends Component {
+    render() {
+        return (
+            <div>
+                Account Setting
+            </div>
+        );
+    }
+}
+
+export default ConfigAccount;
